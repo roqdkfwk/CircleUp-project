8e08d018b9c200b463ef5d38b6af8af240cacb25
q0_u_0p <q0_u_0p@naver.com>
2024-07-23 15:11:17 +0900

    feat: 컨트롤러에 인증 추가

--- backend/src/main/java/com/ssafy/api/controller/RegisterController.java
+++ backend/src/main/java/com/ssafy/api/controller/RegisterController.java
@@ -2,12 +2,14 @@ package com.ssafy.api.controller;
 
 import com.ssafy.api.response.CoursesRes;
 import com.ssafy.api.service.CourseSerivce;
+import com.ssafy.common.custom.AuthFailure;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,42 +23,39 @@ public class RegisterController {
     private final CourseSerivce courseService;
 
     @GetMapping("/courses/registers")
+    @AuthFailure
     @ApiOperation(value = "내가 수강한 강의 목록 조회")
-    @ApiResponses({
-            @ApiResponse(code = 401, message = "인증 실패")
-    })
-    public ResponseEntity<List<CoursesRes>> registeredCourses() {
-        // TODO
-        Long memberId = 3L;
+    public ResponseEntity<List<CoursesRes>> registeredCourses(
+            Authentication authentication
+    ) {
+        Long memberId = Long.valueOf(authentication.getName());
         return ResponseEntity.ok().body(courseService.getCoursesIregistered(memberId));
     }
 
     @GetMapping("/courses/registers/{course_id}")
     @ApiOperation(value = "수강 여부 조회")
-    @ApiResponses({
-            @ApiResponse(code = 401, message = "인증 실패")
-    })
+    @AuthFailure
     public ResponseEntity<Boolean> isRegister(
-            @PathVariable(name = "course_id") Long courseId
+            @PathVariable(name = "course_id") Long courseId,
+            Authentication authentication
     ) {
-        // TODO
-        Long memberId = 3L;
+        Long memberId = Long.valueOf(authentication.getName());
         return ResponseEntity.ok().body(courseService.existRegister(memberId, courseId));
     }
 
 
     @PostMapping("/courses/registers/{course_id}")
     @ApiOperation(value = "수강 신청")
+    @AuthFailure
     @ApiResponses({
-            @ApiResponse(code = 401, message = "인증 실패"),
             @ApiResponse(code = 404, message = "존재하지 않는 회원 혹은 강의"),
             @ApiResponse(code = 409, message = "이미 수강중")
     })
     public ResponseEntity<Void> doRegister(
-            @PathVariable(name = "course_id") Long courseId
+            @PathVariable(name = "course_id") Long courseId,
+            Authentication authentication
     ) {
-        // TODO 인증필터를 이용한 memberId 추가
-        Long memberId = 3L;
+        Long memberId = Long.valueOf(authentication.getName());
         courseService.doRegister(memberId, courseId);
         return ResponseEntity.ok().build();
     }
@@ -64,15 +63,13 @@ public class RegisterController {
 
     @DeleteMapping("/courses/registers/{course_id}")
     @ApiOperation(value = "수강 취소")
-    @ApiResponses({
-            @ApiResponse(code = 401, message = "인증 실패"),
-            @ApiResponse(code = 400, message = "잘못된 요청")
-    })
+    @AuthFailure
+    @ApiResponse(code = 400, message = "잘못된 요청")
     public ResponseEntity<Void> cancelRegister(
-            @PathVariable(name = "course_id") Long courseId
+            @PathVariable(name = "course_id") Long courseId,
+            Authentication authentication
     ) {
-        // TODO
-        Long memberId = 3L;
+        Long memberId = Long.valueOf(authentication.getName());
         courseService.cancelRegister(memberId, courseId);
         return ResponseEntity.ok().build();
     }
