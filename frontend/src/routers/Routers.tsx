a630cdb818993098f98d354e291a5783a63a7169
정현호 <hybrid417@naver.com>
2024-07-31 09:44:51 +0900

    feat: 비디오 페이지 레이아웃 분리

--- frontend/src/routers/Routers.tsx
+++ frontend/src/routers/Routers.tsx
@@ -7,19 +7,19 @@ import CourseManagementMake from "../pages/Instructor/CourseManagementMake";
 import CourseManagementModify from "../pages/Instructor/CourseManagementModify";
 import VideoRoomComponent from "../pages/VideoRoomComponent";
 import GateTemp from "../pages/GateTemp";
+import MainLayout from "../components/MainLayout";
 
 const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/detailCourse" element={<DetailCourse />} />
-      {/* refactoring : /courseManagement:/{ instructorId } <- 전역 상태 관리 라이브러리 활용.. */}
-      <Route path="/courseManagement" element={<CourseManagement />}></Route>
-      <Route path="/courseManagementMake" element={<CourseManagementMake />} />
-      <Route path="/courseManagementModify" element={<CourseManagementModify />} />
-      {/* refactoring : /courseManagementDetail:/{ courseId } */}
-      <Route path="/courseManagementDetail" element={<CourseManagementDetail />} />
-      {/* for WebRTC */}
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Main />} />
+        <Route path="/detailCourse" element={<DetailCourse />} />
+        <Route path="/courseManagement" element={<CourseManagement />} />
+        <Route path="/courseManagementMake" element={<CourseManagementMake />} />
+        <Route path="/courseManagementModify" element={<CourseManagementModify />} />
+        <Route path="/courseManagementDetail" element={<CourseManagementDetail />} />
+      </Route>
       <Route path="/course/live/:course_id" element={<VideoRoomComponent />} />
       <Route path="/course/live/gate" element={<GateTemp />} />
     </Routes>
