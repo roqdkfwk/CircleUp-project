bd72afbb5199ee1f3a9520327196d628c68ef05f
정현호 <hybrid417@naver.com>
2024-08-01 11:06:11 +0900

    feat: add 태그 클릭시 css 효과 부여

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -1,36 +1,50 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { getAllTages } from "../services/api";
 
 interface Tag {
-    tagName: string;
-  }
+  tagName: string;
+}
 
 function Search() {
-    const [CourseTags, setCourseTags] = useState<Tag[]>([])
+  const [CourseTags, setCourseTags] = useState<Tag[]>([]);
+  const [activeTab, setActiveTab] = useState<string>(''); // 활성화된 탭을 관리할 상태
 
-    useEffect(() => {
-        const fetchTags = async() => {
-            const response = await getAllTages();
-            setCourseTags(response.data)
-        }
+  useEffect(() => {
+    const fetchTags = async () => {
+      const response = await getAllTages();
+      setCourseTags(response.data);
+    };
 
-        fetchTags()
-    },[])
+    fetchTags();
+  }, []);
+
+  const handleTabClick = (tagName: string) => {
+    setActiveTab(tagName);
+  };
 
   return (
     <div>
-        <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700 w-[80%] mx-auto mt-5">
-            <ul className="flex flex-wrap -mb-px">
-                {CourseTags?.map((item, idx) => (
-                    <li className="me-2" key={idx}>
-                        <a href="#" className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">{item.tagName}</a>
-                    </li>
-                ))}
-            </ul>
-        </div>
+      <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700 w-[80%] mx-auto mt-5">
+        <ul className="flex flex-wrap -mb-px">
+          {CourseTags?.map((item, idx) => (
+            <li className="me-2" key={idx}>
+              <a
+                href="#"
+                onClick={() => handleTabClick(item.tagName)}
+                className={`inline-block p-4 border-b-2 rounded-t-lg ${
+                  activeTab === item.tagName
+                    ? 'text-blue-600 border-blue-600'
+                    : 'border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300'
+                }`}
+              >
+                {item.tagName}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-  )
-
+  );
 }
 
-export default Search
+export default Search;
