6e0ee3d7ad754b288f7f29a0de07ee4356042454
daunjeong824 <wjdek88@naver.com>
2024-08-02 15:20:13 +0900

    feat: curriculum 설정 (2-1)

--- frontend/src/components/CourseDetail/Content/CourseCurriculumContent.tsx
+++ frontend/src/components/CourseDetail/Content/CourseCurriculumContent.tsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import { getCurriculums } from "../../../services/api";
 import CurriculumList from "../../List/CurriculumList";
+import { CurriculumInfo } from "../../../types/CurriculumInfo";
 
 interface CourseCurriculumContent {
     
@@ -12,11 +14,12 @@ interface CourseCurriculumContent {
 
 const CourseCurriculumContent = ({isLive, isModify, currIds, updateFunc} : CourseCurriculumContent) => {
 
-    const [curriculums, setCurriculums]
+    const [curriculums, setCurriculums] = useState<CurriculumInfo[]>([]);
 
     // <Todo> : fetch curriculum : GET
     const handleGetCurriculums = async () => {
         const response = await getCurriculums(currIds);
+        setCurriculums(response.data);
     }
     // 해당 데이터를 CurriculumList 로 보내기..
     // 라이브 여부 표시 -> 라이브 변수는 CourseStatusBoard -> DetailCourse -> <Course/> 후, 표시!
@@ -24,7 +27,7 @@ const CourseCurriculumContent = ({isLive, isModify, currIds, updateFunc} : Cours
     return (
         <div>
             <div>
-                <CurriculumList />
+                <CurriculumList data={curriculums} />
             </div>
             {/* 강사면 courseManagementModfiy 눌렀을 때, 추가 버튼 사용할 수 있도록 설정 */}
             This is Curriculum Page.
--- frontend/src/components/List/CurriculumList.tsx
+++ frontend/src/components/List/CurriculumList.tsx
@@ -1,6 +1,10 @@
 import { CurriculumInfo } from "../../types/CurriculumInfo";
 
-const CurriculumList = ({ data } : CurriculumInfo[]) => {
+interface CurriculumListprops {
+    data : CurriculumInfo[],
+}
+
+const CurriculumList = ({ data } : CurriculumListprops) => {
     return (
         <div>
             <h1 className="mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-2xl lg:text-2xl dark:text-white">
