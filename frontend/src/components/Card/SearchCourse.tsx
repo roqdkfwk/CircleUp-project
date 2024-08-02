ca00e97b9042b376d5d90aaf0059114f94387fc8
정현호 <hybrid417@naver.com>
2024-08-02 09:30:20 +0900

    feat: 검색 기능 UI 수정

--- frontend/src/components/Card/SearchCourse.tsx
+++ frontend/src/components/Card/SearchCourse.tsx
@@ -1,37 +1,41 @@
 import { useNavigate } from "react-router-dom";
 import { CourseDetailInfoBySearch } from '../../types/CourseDetailInfoBySearch';
+import { FaUser, FaWonSign, FaEye  } from "react-icons/fa";
 
 interface SearchCourseProps {
     data: CourseDetailInfoBySearch;
 }
 
 const SearchCourse = ({ data } : SearchCourseProps ) => {
-
+    const formattedPrice = data.price === 0 ? "무료" : data.price.toLocaleString();
     const navigate = useNavigate();
 
-    function navigateToMyCourseDetail() {
+    function navigateToCourseDetail() {
         navigate(`/courseDetail/${data.id}`);
     }
 
     return (
-        <div className="h-auto max-w-full rounded-lg max-w-sm w-[200px] h-[280px] bg-white rounded-lg shadow mx-[10px]"
-            onClick={navigateToMyCourseDetail}>
+        <div className="max-w-full min-h-[330px] rounded-lg max-w-sm w-[250px] bg-white rounded-lg border mx-[10px] cursor-pointer"
+            onClick={navigateToCourseDetail}>
             
-            <a href="#" onClick={(e) => {
-                e.preventDefault();
-                navigateToMyCourseDetail();
-            }}>
-                <img className="rounded-t-lg mx-auto w-full h-[150px] " src={data.imgUrl} alt="" />
-            </a>
+            <img className="rounded-t-lg mx-auto w-full h-[150px] " src={data.imgUrl} alt="" />
         
-            <div className="p-5">
-                <a href="#" onClick={(e) => {
-                    e.preventDefault();
-                    navigateToMyCourseDetail();
-                }}>
-                    <h5 className="mb-2 text-base font-bold tracking-tight text-gray-900 dark:text-white">{data.name}</h5>
-                </a>
-                <p className="mb-3 text-sm text-gray-700 dark:text-gray-400">수강생 출력 필요</p>
+            <div className="px-5 py-3">
+                <div className="w-full flex"> 
+                    {data.tags.map((tagName, idx) => (
+                        <span className="bg-indigo-100 text-indigo-800 text-sm me-2 mt-[2px] px-2.5 rounded dark:bg-indigo-900 dark:text-indigo-300" key={idx}>{tagName}</span>
+                    ))}
+                </div>
+                <h5 className="text-base font-bold tracking-tight GD my-[10px] min-h-[50px] text-gray-800 dark:text-white">{data.name}</h5>
+                <div className="text-gray-500 text-sm flex items-center">
+                    <FaUser style={{color: 'gray'}} /><p className="ml-2">{data.registeredCnt}</p>
+                </div>
+                <div className="text-gray-500 text-sm flex items-center">
+                    <FaWonSign style={{color: 'gray'}} /><p className="ml-2">{formattedPrice}</p>
+                </div>
+                <div className="text-gray-500 text-sm flex items-center">
+                    <FaEye style={{color: 'gray'}} /><p className="ml-2">{data.view}</p>
+                </div>
             </div>
         </div>
     );
