7a4b01e04a786a3500413158f28da9326effc2b1
daunjeong824 <wjdek88@naver.com>
2024-08-12 13:51:30 +0900

    feat: Comment 구현 (2) - 삭제 기능 구현

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -173,15 +173,27 @@ export const getComment = (courseId: number, reviewId: number) => {
 }
 // CREATE Comment
 export const postComment = (courseId: number, content : Record<string, unknown>) => {
-    return axiosClient.post(`courses/${courseId}/reviews`, content,);
+    return axiosClient.post(`courses/${courseId}/reviews`, content, {
+        headers: {
+            'Requires-Auth': true,
+        }
+    });
 }
 // UPDATE Comment
 export const patchComment = (courseId: number, reviewId: number, content: Record<string, unknown>) => {
-    return axiosClient.patch(`courses/${courseId}/reviews/${reviewId}`, content,);
+    return axiosClient.patch(`courses/${courseId}/reviews/${reviewId}`, content, {
+        headers: {
+            'Requires-Auth': true,
+        }
+    });
 }
 // DELETE Comment
 export const deleteComment = (courseId: number, reviewId: number) => {
-    return axiosClient.delete(`courses/${courseId}/reviews/${reviewId}`,)
+    return axiosClient.delete(`courses/${courseId}/reviews/${reviewId}`, {
+        headers: {
+            'Requires-Auth': true,
+        }
+    })
 }
 
 /* Member API */
