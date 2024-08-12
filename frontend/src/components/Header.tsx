02fd451755c6e427e579d4c04e62ff0858310516
정현호 <hybrid417@naver.com>
2024-08-12 13:44:16 +0900

    chore: 태그 정렬

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -242,7 +242,7 @@ function Header() {
               >
                 로그인
                 </button>
-                <button
+              <button
                 onClick={toggleSignUpModal}
                 type="button"
                 className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3"
