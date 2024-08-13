b55535a68992a9ff0c9120f8759034214ec4e851
q0_u_0p <q0_u_0p@naver.com>
2024-08-13 10:20:36 +0900

    refactor: Live에서 안쓰는 API 제거

--- backend/src/main/java/com/ssafy/api/controller/SessionController.java
+++ backend/src/main/java/com/ssafy/api/controller/SessionController.java
@@ -17,8 +17,10 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
 import java.io.InputStream;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @Api(tags = {"Live"})
 @CrossOrigin(origins = "*")
@@ -162,109 +164,6 @@ public class SessionController {
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-
-    /////////////////////////////////////////////////////
-
-    /**
-     * 개발용
-     * Live 방 개설
-     */
-    @PostMapping("/dev/{course_id}")
-    @ApiOperation(value = "(개발용) Live 방 개설", notes = "누구나 존재하지 않는 강의번호로 Live를 개설할 수 있습니다")
-    @ApiResponses({
-            @ApiResponse(code = 406, message = "사용할 수 없는 강의번호")
-    })
-    public ResponseEntity<String> initializeSession(
-            @PathVariable(name = "course_id") String courseId
-    ) {
-        if (courseSerivce.existsCourse(Long.valueOf(courseId))) {
-            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
-        }
-        makeSession(courseId);
-        return new ResponseEntity<>(courseId, HttpStatus.OK);
-    }
-
-    /**
-     * 개발용
-     * Live 종료
-     */
-    @DeleteMapping("/dev/{course_id}")
-    @ApiOperation(value = "(개발용) Live 종료", notes = "라이브를 종료하고 화면녹화파일을 (로컬에) 저장한다")
-    public ResponseEntity<ArrayList<String>> recordingList(
-            @PathVariable("course_id") String courseId
-    ) throws OpenViduJavaClientException, OpenViduHttpException {
-        openvidu.getActiveSession(courseId).close();
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    /**
-     * 개발용
-     * Live 방 접속
-     */
-    @PostMapping("/dev/{course_id}/connections")
-    @ApiOperation(value = "(개발용) Live 방 접속", notes = "누구나 라이브 중인 강의로 접속할 수 있습니다")
-    @ApiResponses({
-            @ApiResponse(code = 404, message = "라이브 강의를 찾을 수 없음"),
-            @ApiResponse(code = 500, message = "세션 생성 에러")
-    })
-    public ResponseEntity<String> initializeConnection(
-            @PathVariable("course_id") String courseId
-    ) throws OpenViduJavaClientException, OpenViduHttpException {
-        Session session = openvidu.getActiveSession(courseId);
-        if (session == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        ConnectionProperties properties = new ConnectionProperties.Builder().build();
-        Connection connection = session.createConnection(properties);
-        return new ResponseEntity<>(connection.getToken(), HttpStatus.OK);
-    }
-
-    /**
-     * 개발용
-     * Live 방 목록 반환
-     */
-    @GetMapping("/dev/courses")
-    @ApiOperation(value = "(개발용) Live 방 목록", notes = "현재 라이브 중인 강의 목록을 반환합니다")
-    public ResponseEntity<ArrayList<String>> sessionList(
-    ) {
-        ArrayList<String> sessions = new ArrayList<>();
-        for (Session session : openvidu.getActiveSessions()) {
-            sessions.add(session.getSessionId());
-        }
-        return new ResponseEntity<>(sessions, HttpStatus.OK);
-    }
-
-    ///////////////////////////////////////////////////// 녹화 파일 관리용
-
-    @GetMapping("/dev/recording")
-    @ApiOperation(value = "(개발용) 녹화 파일 목록", notes = "현재 저장된 녹화 파일 목록을 반환합니다")
-    public ResponseEntity<?> recordingList(
-    ) throws OpenViduJavaClientException, OpenViduHttpException {
-        List<Recording> recordings =
-                openvidu.listRecordings();
-
-        List<String> recordingIds = recordings.stream().map(recording -> recording.getId()).collect(Collectors.toList());
-        return new ResponseEntity<>(recordingIds, HttpStatus.OK);
-    }
-
-    @PostMapping("/dev/recording")
-    @ApiOperation(value = "(개발용) 녹화 중지", notes = "녹화를 중지시킵니다")
-    public ResponseEntity<Void> recordingStop(
-            @RequestParam(name = "course_id") String courseId
-    ) throws OpenViduJavaClientException, OpenViduHttpException {
-        openvidu.stopRecording(courseId);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @DeleteMapping("/dev/recording")
-    @ApiOperation(value = "(개발용) 녹화 삭제", notes = "녹화된 파일을 삭제합니다")
-    public ResponseEntity<Void> recordingDelete(
-            @RequestParam(name = "course_id") String courseId
-    ) throws OpenViduJavaClientException, OpenViduHttpException {
-        openvidu.deleteRecording(courseId);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
