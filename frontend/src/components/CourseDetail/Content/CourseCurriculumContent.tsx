dfb2dd0b6b72275315d8d944088cf9a0cff2f6ac
q0_u_0p <q0_u_0p@naver.com>
2024-08-14 10:43:07 +0900

    style: 커리큘럼 목록 수정

--- frontend/src/components/CourseDetail/Content/CourseCurriculumContent.tsx
+++ frontend/src/components/CourseDetail/Content/CourseCurriculumContent.tsx
@@ -5,44 +5,43 @@ import { CurriculumInfo } from "../../../types/CurriculumInfo";
 import CurriculumMakeModal from "../../Modal/CurriculumMakeModal";
 
 interface CourseCurriculumContent {
-
-    isModify: string,
-    courseId: number,
+  isModify: string;
+  courseId: number;
 }
 
-const CourseCurriculumContent = ({isModify, courseId} : CourseCurriculumContent) => {
+const CourseCurriculumContent = ({ isModify, courseId }: CourseCurriculumContent) => {
+  const [curriculums, setCurriculums] = useState<CurriculumInfo[]>([]);
+  const [showModal, setShowModal] = useState<boolean>(false);
 
-    const [curriculums, setCurriculums] = useState<CurriculumInfo[]>([]);
-    const [showModal, setShowModal] = useState<boolean>(false);
-    
-    //const [myCurriIds, setMyCurriIds] = useState<number[]>([]);
-    const toggleModal = () => {
-        setShowModal(!showModal);
-    }
+  //const [myCurriIds, setMyCurriIds] = useState<number[]>([]);
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
 
-    // <Todo> : fetch curriculum : GET
-    const handleGetCurriculums = async () => {
-        const response = await getCurriculums(courseId);
-        setCurriculums(response.data);
-    }
-    
-    // const handleCurris = (newCurris : CurriculumInfo[]) => {
-    //     setCurriculums([...newCurris])
-    // }
+  // <Todo> : fetch curriculum : GET
+  const handleGetCurriculums = async () => {
+    const response = await getCurriculums(courseId);
+    setCurriculums(response.data);
+  };
 
-    useEffect(() => {
-        handleGetCurriculums();
-    }, [courseId])
-    
-    return (
-        <div className="flex flex-row items-center">
-            <CurriculumMakeModal show={showModal} onClose={toggleModal} courseId={courseId} />
-            <div>
-                <CurriculumList data={curriculums} isModfy={isModify} courseId={courseId} />
-            </div>
-            {isModify === "instructorModify" &&
-                <button type="button"
-                    className="
+  // const handleCurris = (newCurris : CurriculumInfo[]) => {
+  //     setCurriculums([...newCurris])
+  // }
+
+  useEffect(() => {
+    handleGetCurriculums();
+  }, [courseId]);
+
+  return (
+    <div className="flex flex-row items-center">
+      <CurriculumMakeModal show={showModal} onClose={toggleModal} courseId={courseId} />
+      <div>
+        <CurriculumList data={curriculums} isModfy={isModify} courseId={courseId} />
+      </div>
+      {isModify === "instructorModify" && (
+        <button
+          type="button"
+          className="
                     w-1/2 h-1/2
                     text-white bg-blue-700 hover:bg-blue-800 
                     focus:ring-4 focus:ring-blue-300 
@@ -50,14 +49,14 @@ const CourseCurriculumContent = ({isModify, courseId} : CourseCurriculumContent)
                     text-sm px-4 py-2.5 mx-10 my-10
                     focus:outline-none dark:focus:ring-blue-800
                     "
-                    onClick={toggleModal}
-                >
-                    커리큘럼 생성
-                </button>
-            }
-           {/* 기타 데이터 나타내기 */}
-        </div>
-    )
-}
+          onClick={toggleModal}
+        >
+          커리큘럼 생성
+        </button>
+      )}
+      {/* 기타 데이터 나타내기 */}
+    </div>
+  );
+};
 
 export default CourseCurriculumContent;
